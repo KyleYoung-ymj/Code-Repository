9c8a4d972275e82dd67058dea5d88104badb816a
KyleYoung-ymj <kyleyoung_ymj@163.com>
2016-10-21 14:40:16 +0800

    update
    Oct.20.16

--- AC/POI/POI2014 Around the world.cpp
+++ AC/POI/POI2014 Around the world.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #define fi first
 #define se second
+#define y0 vjfuyg
 #define y1 jfskav
 #define pb push_back
 #define lson (k<<1)
@@ -22,102 +23,97 @@ typedef vector<int> veci;
 const int inf=0x7fffffff,mod=(int)1e9+7,dxy[]={-1,0,1,0,-1};
 const ll INF=1ll<<60;
 const db pi=acos(-1),eps=1e-6;
-template<class T>void rd(T &x){
+template<class T>void rd(T &x)
+{
 	x=0;
 	char c;
 	while(c=getchar(),c<48);
 	do x=(x<<3)+(x<<1)+(c^48);
 		while(c=getchar(),c>47);
 }
-template<class T>void rec_pt(T x){
+template<class T>void rec_pt(T x)
+{
 	if(!x)return;
 	rec_pt(x/10);
 	putchar(x%10^48);
 }
-template<class T>void pt(T x){
+template<class T>void pt(T x)
+{
 	if(!x)putchar('0');
 	else rec_pt(x);
 }
-template<class T>inline void ptn(T x){
+template<class T>inline void ptn(T x)
+{
 	pt(x),putchar('\n');
 }
-template<class T>inline void Max(T &a,T b){
+template<class T>inline void Max(T &a,T b)
+{
 	if(b>a)a=b;
 }
-template<class T>inline void Min(T &a,T b){
+template<class T>inline void Min(T &a,T b)
+{
 	if(b<a)a=b;
 }
-template<class T>T gcd(T a,T b){
+template<class T>T gcd(T a,T b)
+{
 	return b?gcd(b,a%b):a;
 }
-inline void mod_add(int &a,int b,int m=mod){
+inline void mod_add(int &a,int b,int m=mod)
+{
 	if((a+=b)>=m)a-=m;
 }
-inline void mod_minus(int &a,int b,int m=mod){
+inline void mod_minus(int &a,int b,int m=mod)
+{
 	if((a-=b)<0)a+=m;
 }
-int mod_pow(int a,int b,int m=mod){
+int mod_pow(int a,int b,int m=mod)
+{
 	int res=1;
 	for(;b;b>>=1,a=(ll)a*a%m)
 		if(b&1)res=(ll)res*a%m;
 	return res;
 }
-inline int calc_inv(int x,int m=mod){
+inline int calc_inv(int x,int m=mod)
+{
 	return mod_pow(x,m-2);
 }
 
 
-const int N=(int)2e6+5;
+const int N=(int)1e6+5<<1;
 
-int dist[N],nxt[N],par[N],dep[N],arr[N];
+int arr[N],pre[N],cnt[N];
+ll sum[N];
 
-int get_root(int x){
-	return par[x]==x?x:par[x]=get_root(par[x]);
-}
-
-int main(){
-	int size = 32 << 20; // 32MB
-	char *p = (char*)malloc(size) + size;
-	__asm__("movl %0, %%esp\n" :: "r"(p));
-	freopen("doo16a.in","r",stdin);//
-	freopen("data.out","w",stdout);//
+int main()
+{
 	int n,m,mx=0;
 	rd(n),rd(m);
-	int _n=n<<1;
-	rep(i,1,_n+1){
-		int j=i+1;
-		if(i<=n){
-			rd(dist[j]);
-			Max(mx,dist[j]);
-		}
-		else dist[j]=dist[j-n]-dist[i-n];
-		dist[j]+=dist[i];
+	rep(i,1,n+1)
+	{
+		rd(arr[i]);
+		Max(mx,arr[i]);
+		sum[i]=sum[i-1]+arr[i];
+		arr[i+n]=arr[i];
 	}
-	nxt[_n]=par[_n]=_n;
-	for(int K;m--;){
-		rd(K);
-		if(K<mx)puts("NIE");
-		else{
-			per(i,1,_n){
-				int &res=nxt[i];
-				for(res=nxt[i+1];dist[res]-dist[i]>K;--res);
-				par[i]=i;
-				dep[i]=dep[res]+1;
+	rep(i,n+1,(n<<1)+1)sum[i]=sum[i-1]+arr[i];
+	for(ll lim;m--;)
+	{
+		rd(lim);
+		if(lim<mx)
+		{
+			puts("NIE");
+			continue;
+		}
+		rep(i,0,(n<<1)+1)pre[i]=i,cnt[i]=0;
+		for(int i=n,j=0;;++i)
+		{
+			for(;sum[i]-sum[j]>lim;++j);
+			cnt[i]=cnt[j]+1;
+			if(i-(pre[i]=pre[j])>=n)
+			{
+				ptn(cnt[i]);
+				break;
 			}
-			int ans=inf;
-			rep(i,1,n+1){
-				int tot=0;
-				for(int u=get_root(i);;){
-					if(u>=i+n){
-						Min(ans,dep[i]-dep[u]);
-						rep(i,0,tot)par[arr[i]]=u;
-						break;
-					}
-					arr[tot++]=u;
-					u=get_root(nxt[get_root(u)]);
-				}
-			}
-			ptn(ans);
 		}
 	}
 	return 0;
@@ -125,14 +121,12 @@ int main(){
 
 /*
 	
-	Oct.06.16
+	Oct.19.16
 	
-	Tags:implementation,dsu
-	Submissions:2
+	Tags:implementation
 	
-	Memory 64076KB
-	Time 8696MS
-	Code Length 2623B
+	Memory 40764KB
+	Time 2288MS
+	Code Length 2493B
 	
 */
-
--- AC/POI/POI2014 Salad Bar.cpp
+++ AC/POI/POI2014 Salad Bar.cpp
@@ -1,6 +1,22 @@
-#include <bits/stdc++.h>
+#include <cmath>
+#include <ctime>
+#include <cctype>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <bitset>
+#include <complex>
+#include <iostream>
+#include <algorithm>
 #define fi first
 #define se second
+#define y0 vjfuyg
 #define y1 jfskav
 #define pb push_back
 #define lson (k<<1)
@@ -22,47 +38,58 @@ typedef vector<int> veci;
 const int inf=0x7fffffff,mod=(int)1e9+7,dxy[]={-1,0,1,0,-1};
 const ll INF=1ll<<60;
 const db pi=acos(-1),eps=1e-6;
-template<class T>void rd(T &x){
+template<class T>void rd(T &x)
+{
 	x=0;
 	char c;
 	while(c=getchar(),c<48);
 	do x=(x<<3)+(x<<1)+(c^48);
 		while(c=getchar(),c>47);
 }
-template<class T>void rec_pt(T x){
+template<class T>void rec_pt(T x)
+{
 	if(!x)return;
 	rec_pt(x/10);
 	putchar(x%10^48);
 }
-template<class T>void pt(T x){
+template<class T>void pt(T x)
+{
 	if(!x)putchar('0');
 	else rec_pt(x);
 }
-template<class T>inline void ptn(T x){
+template<class T>inline void ptn(T x)
+{
 	pt(x),putchar('\n');
 }
-template<class T>inline void Max(T &a,T b){
+template<class T>inline void Max(T &a,T b)
+{
 	if(b>a)a=b;
 }
-template<class T>inline void Min(T &a,T b){
+template<class T>inline void Min(T &a,T b)
+{
 	if(b<a)a=b;
 }
-template<class T>T gcd(T a,T b){
+template<class T>T gcd(T a,T b)
+{
 	return b?gcd(b,a%b):a;
 }
-inline void mod_add(int &a,int b,int m=mod){
+inline void mod_add(int &a,int b,int m=mod)
+{
 	if((a+=b)>=m)a-=m;
 }
-inline void mod_minus(int &a,int b,int m=mod){
+inline void mod_minus(int &a,int b,int m=mod)
+{
 	if((a-=b)<0)a+=m;
 }
-int mod_pow(int a,int b,int m=mod){
+int mod_pow(int a,int b,int m=mod)
+{
 	int res=1;
 	for(;b;b>>=1,a=(ll)a*a%m)
 		if(b&1)res=(ll)res*a%m;
 	return res;
 }
-inline int calc_inv(int x,int m=mod){
+inline int calc_inv(int x,int m=mod)
+{
 	return mod_pow(x,m-2);
 }
 
@@ -70,53 +97,40 @@ inline int calc_inv(int x,int m=mod){
 const int N=(int)1e6+5;
 
 char str[N];
-int n,sum[N],pos[N<<1],L[N],R[N],cnt[N],arr[N],bit[N];
+int sum[N],R[N],pos[N<<1];
 
-inline void modify(int x){
-	int val=x;
-	for(;x<=n;x+=lowbit(x))Max(bit[x],val);
-}
-int query_mx(int x){
-	int res=0;
-	for(;x;x-=lowbit(x))Max(res,bit[x]);
-	return res;
-}
-
-int main(){
+int main()
+{
+	int n,ans=0;
 	rd(n);
 	scanf("%s",str+1);
-	rep(i,1,n+1)sum[i]=sum[i-1]+(str[i]=str[i]=='p'?1:-1);
-	rep(i,1,n+1){
-		L[i]=pos[sum[i]+1+N]+1;
-		pos[sum[i]+N]=i;
-		++cnt[L[i]];
-	}
-	rep(i,0,N<<1)pos[i]=n+1;
-	pos[sum[n]+N]=n;
-	per(i,0,n){
-		R[i+1]=pos[sum[i]-1+N]-1;
-		pos[sum[i]+N]=i;
-	}
-	rep(i,2,n+1)cnt[i]+=cnt[i-1];
-	rep(i,1,n+1)arr[--cnt[L[i]]]=i;
-	int ans=0;
-	for(int i=1,j=0;i<=n;++i){
-		for(;j<n&&L[arr[j]]==i;++j)modify(arr[j]);
-		Max(ans,query_mx(R[i])-i+1);
+	sum[0]=N;
+	rep(i,1,n+1)sum[i]=sum[i-1]+(str[i]=='p'?1:-1);
+	R[n]=pos[sum[n]]=n;
+	per(i,0,n)
+	{
+		if(sum[i]<sum[i+1])
+		{
+			R[i]=R[i+1];
+			if(pos[sum[i]]&&sum[R[pos[sum[i]]]]>=sum[R[i]])R[i]=R[pos[sum[i]]];
+		}
+		else R[i]=i;
+		Max(ans,R[i]-i);
+		pos[sum[i]]=i;
 	}
 	ptn(ans);
 	return 0;
 }
+
 /*
 	
-	Oct.04.16
+	Oct.19.16
 	
-	Tags:data structure,bit
-	Submissions:1
+	Tags:implementation
 	
-	Memory 33928KB
-	Time 132MS
-	Code Length 2714B
+	Memory 18300KB
+	Time 20MS
+	Code Length 2643B
 	
 */
 
